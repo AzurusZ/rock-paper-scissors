9d56434ceeda412cb9bc40eec001167e5314d7df
Zhele134 <roxas22002@gmail.com>
2021-01-23 16:05:59 -0700

    Finished the first final draft of the webpage.

--- game.js
+++ game.js
@@ -1,9 +1,11 @@
-
 const resultText = document.querySelector(".resultText");
 const rockButton = document.querySelector(".playerRock");
 const paperButton = document.querySelector(".playerPaper");
 const scissorsButton = document.querySelector(".playerScissors");
 const restartButton = document.querySelector(".tryAgainButton");
+
+const playerScoreBorder = document.querySelector("#playerScoreContainer");
+const computerScoreBorder = document.querySelector("#computerScoreContainer");
 
 let playerPoints = 0;
 let computerPoints = 0;
@@ -30,32 +32,41 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock") {
         if (computerSelection === "rock") {
             resultText.textContent = "It's a tie! Both chose Rock";
+            highlightWinner("tie");
         } else if (computerSelection === "paper") {
             computerPoints++;
             resultText.textContent = "You lose! Paper beats Rock";
+            highlightWinner("computer");
         } else if (computerSelection === "scissors") {
             playerPoints++;
             resultText.textContent = "You win! Rock beats Scissors";
+            highlightWinner("player");
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
             playerPoints++;
             resultText.textContent = "You win! Paper beats Rock";
+            highlightWinner("player");
         } else if (computerSelection === "paper") {
             resultText.textContent = "It's a tie! Both chose paper";
+            highlightWinner("tie");
         } else if (computerSelection === "scissors") {
             computerPoints++;
             resultText.textContent = "You lose! Scissors beats paper";
+            highlightWinner("computer");
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
             computerPoints++;
             resultText.textContent = "You lose! Scissors beats Rock";
+            highlightWinner("computer");
         } else if (computerSelection === "paper") {
             playerPoints++;
             resultText.textContent = "You win! Scissors beats paper";
+            highlightWinner("player");
         } else if (computerSelection === "scissors") {
             resultText.textContent = "It's a tie! Both chose Scissors";
+            highlightWinner("tie");
         }
     } else {
         resultText.textContent = "Whoops! Something went wrong!";
@@ -97,7 +108,28 @@ function restartGame() {
     playerPoints = 0;
     computerPoints = 0;
     resultText.textContent = "Select a choice to start";
+    playerScoreBorder.style.borderColor = "black";
+    computerScoreBorder.style.borderColor = "black";
     updatePoints();
+}
+
+function highlightWinner(winner) {
+    switch(winner) {
+        case "player":
+            playerScoreBorder.style.borderColor = "#42F58A";
+            computerScoreBorder.style.borderColor = "black";
+            break;
+        case "computer":
+            playerScoreBorder.style.borderColor = "black";
+            computerScoreBorder.style.borderColor = "#42F58A";
+            break;
+        case "tie":
+            playerScoreBorder.style.borderColor = "black";
+            computerScoreBorder.style.borderColor = "black";
+            break;
+        default:
+            break;
+    }
 }
 
 rockButton.addEventListener("click", startRound);
